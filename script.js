a7bfd5a3533662b74f3467965d6d2e1cce8017b6
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-22 17:09:08 -0500

    scale down user story; remove subjects

--- script.js
+++ script.js
@@ -2,35 +2,56 @@ var board = document.getElementById('board');
 var player = document.getElementById('player');
 var nav = document.getElementById('nav');
 var form =document.getElementById('form');
-var questionsDiv = document.querySelector('flex-item');
+var questionsDiv = document.querySelector('flex-item'); // pause here to test and change to jquery after testing
 var answersDiv = $("flex-item:nth-child(2)");
-// document.querySelector('')
+// document.querySelector('') : use the jquery first
 var countersDiv = $("flex-item:nth-child(3)");
-
+ // get questions and answers from preloaded subject arrays
 var scienceQuestion = {
   scienceQue: function(){
-    for (var i = 0; i <questions.length; i++)
-    console.log('answers');
+    for (var i = 0; i <science.questions.length; i++)
+    console.log('questions');
   }
 var scienceAnswers = {
   scienceAns: function(){
-    for (var i = 0; i <answers.length; i++)
+    for (var i = 0; i <science.choices.length; i++)
     console.log('answers');
   }
-var artsQuestions = science[i];
-var artsAnswers = arts[1][i]
-science{
-    question: answers[0,1,2],
-    question2: answer[0,1,2]
+var artsQuestions = {
+  artsQue: function(){
+    for (var i = 0; i <arts.questions.length; i++)
+    console.log('answers');
+  }
+var artsAnswers = {
+  artsAns: function(){
+    for (var i = 0; i <arts.choices.length; i++)
+    console.log('answers');
+  }
+
+var programmingQuestions = {
+  programQue: function(){
+    for (var i = 0; i <program.questions.length; i++)
+    console.log('answers');
+  }
+  var programmingAnswers = {
+    programAns: function(){
+      for (var i = 0; i <program.choices.length; i++)
+      console.log('answers');
+    }
+
+science = {
+    questions = [
+      { question: "What size is",
+        choices: [10,44,15],
+        correctIndex: 1
+      },
+      { question: "???"
+        choices: [0,1,2]
+        correctIndex: 2
+      }
+  ]
 }
-arts: {
-  question: answers[0,1,2],
-  question2: answer[0,1,2]
-}
-programming: {
-  question: answers[0,1,2],
-  question2: answer[0,1,2]
-}
+
 var playTrivia = {
 
 }
