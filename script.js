491eb30b4ce8536d13ff1f681ccdfcdc0234d87d
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-22 16:34:39 -0500

    started playing with js

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ var board = document.getElementById('board');
 var player = document.getElementById('player');
 var nav = document.getElementById('nav');
 var form =document.getElementById('form');
+var questionsDiv = document.querySelector('flex-item');
+var answersDiv = $("flex-item:nth-child(2)");
+// document.querySelector('')
+var countersDiv = $("flex-item:nth-child(3)");
 
+var scienceQuestion = {
+  scienceQue: function(){
+    for (var i = 0; i <questions.length; i++)
+    console.log('answers');
+  }
+var scienceAnswers = {
+  scienceAns: function(){
+    for (var i = 0; i <answers.length; i++)
+    console.log('answers');
+  }
+var artsQuestions = science[i];
+var artsAnswers = arts[1][i]
+science{
+    question: answers[0,1,2],
+    question2: answer[0,1,2]
+}
+arts: {
+  question: answers[0,1,2],
+  question2: answer[0,1,2]
+}
+programming: {
+  question: answers[0,1,2],
+  question2: answer[0,1,2]
+}
 var playTrivia = {
+
 }
