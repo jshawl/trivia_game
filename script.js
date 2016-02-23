6284835fe446906049c5a9a317f1367e9656f4d9
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-23 15:02:32 -0500

    created functions to iterate through Q&A

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
-var board = document.getElementById('board');
 var player = document.getElementById('player');
-var nav = document.getElementById('nav');
 var form =document.getElementById('form');
+
 var questionsDiv = document.querySelector('flex-item'); // pause here to test and change to jquery after testing
-var answersDiv = $("flex-item:nth-child(2)");
-// document.querySelector('') : use the jquery first
-var countersDiv = $("flex-item:nth-child(3)");
+var answersDiv = document.querySelectorAll("flex-item:nth-child(2)");
+// // document.querySelector('') : use vanilla first
+var countersDiv = document.querySelectorAll("flex-item:nth-child(3)");
  // get questions and answers from preloaded subject arrays
-var scienceQuestion = {
-  scienceQue: function(){
-    for (var i = 0; i <science.questions.length; i++)
-    console.log('questions');
-  }
-var scienceAnswers = {
-  scienceAns: function(){
-    for (var i = 0; i <science.choices.length; i++)
-    console.log('answers');
-  }
-var artsQuestions = {
-  artsQue: function(){
-    for (var i = 0; i <arts.questions.length; i++)
-    console.log('answers');
-  }
-var artsAnswers = {
-  artsAns: function(){
-    for (var i = 0; i <arts.choices.length; i++)
-    console.log('answers');
-  }
-
-var programmingQuestions = {
-  programQue: function(){
-    for (var i = 0; i <program.questions.length; i++)
-    console.log('answers');
-  }
-  var programmingAnswers = {
-    programAns: function(){
-      for (var i = 0; i <program.choices.length; i++)
-      console.log('answers');
+ var scienceQuestion = {
+scienceQue: function() {
+    for (var i = 0; i < this.science.length; i++) {
+        questionsDiv.innerHTML +=
+         this.science[i].question;
     }
+},
+scienceChoices: function() {
+    for (var i = 0; i < this.science.length; i++) {
+        questionsDiv.innerHTML += this.science[i].choices;
+    }
+},
 
-science = {
-    questions = [
-      { question: "What size is",
-        choices: [10,44,15],
-        correctIndex: 1
-      },
-      { question: "???"
-        choices: [0,1,2]
-        correctIndex: 2
-      }
-  ]
+science: [
+    { question: "What size is", choices: [10, 44, 15], correctIndex: 1 },
+    { question: "???", choices: [0, 1, 2], correctIndex: 2 },
+    { question: "Where is ", choices: [10, 44, 15], correctIndex: 1 },
+    { question: "How do I", choices: [10, 44, 15], correctIndex: 0 }
+]
 }
 
-var playTrivia = {
 
-}
+
+
+
+
+
+
+
+// var playTrivia = {
+//
+// };
