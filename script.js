492c8c0f8be999f9b60b10ab13aa4dc984bf7a1d
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-24 16:16:58 -0500

    evenlistener added to button and tracked by class

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
+
 var player = document.getElementById('player');
 var form =document.getElementById('form');
-
-var questionsDiv = document.querySelectorAll(".flex-item:nth-child(1)"); // pause here to test and change to jquery after testing
-var answersDiv = document.querySelectorAll(".flex-item:nth-child(2)");
-// // document.querySelector('') : use vanilla first
-var countersDiv = document.querySelectorAll(".flex-item:nth-child(3)");
- // get questions and answers from preloaded subject arrays
+var questionsDiv = document.getElementById("question");
+var choicesDiv = document.getElementById("choices");
+var countersDiv = document.getElementById("counter");
 
 var playTrivia = {
+  totalAnswerCorrect: 0,
+  totalQuestionsAsked: 0,
   counter: 0,
   // display questions
   triviaQue: function() {
         var questions = this.trivia[this.counter].question;
-        questionsDiv[0].innerHTML =  questions;
+        questionsDiv.innerHTML =  questions;
   },
-
 // display choices
   triviaChoices: function() {
-
+    // Loop through, attach button and print each choice
         for (var j=0; j<this.trivia[this.counter].choices.length; j++) {
-          answersDiv[0].innerHTML += "<input type='radio' name='myRadio' />" + this.trivia[this.counter].choices[j]
-          // attach button to each choice
+          choicesDiv.innerHTML += "<input type='radio' class="+ j + " name='myRadio' />" + this.trivia[this.counter].choices[j]
+        }
+        var answer = this.trivia[this.counter].answerIndex
+        // add eventHandlers for all myRadio
+        for (var i=0; i< choicesDiv.children.length; i++){
+          console.log(choicesDiv)
+          console.log(choicesDiv.children[i])
+          choicesDiv.children[i].addEventListener("click", playTrivia.triviaAnswer)
         }
   },
-
   triviaAnswer: function(){
-    var playerChoice = [answersDiv[0].elements['myRadio']];
-    for (var i=0; i < playerChoice.length; i++) {
-      playerChoice[i].checked = function() {
-        console.log(playerChoice.index)
-        // if (this.trivia[this.counter].answerIndex = playerChoice.index)
-        //playerChoice
-      }
-    }
+    console.log(this.children)
+    // var playerChoice = [choicesDiv[0].elements['myRadio']];
+    var playerChoiceRadio = choicesDiv.children
+    console.log("clicked radio:", playerChoiceRadio)
+    var playerChoice = playerChoiceRadio //???
+    console.log("playerChoice:", playerChoice)
+
+      // if (playerChoice = this.answer){
+      //   totalAnswerCorrect++;
+      // }
+
+
+    // for (var i=0; i < playerChoice.length; i++) {
+    //   playerChoice[i].checked = function() {
+    //     console.log(playerChoice.index)
+    //     // if (this.trivia[this.counter].answerIndex = playerChoice.index)
+    //     //playerChoice
+    //   }
+    // }
     // if user click answerIndex button, increase number of correct answers by 1, else increae number of incorrect annswer by 1 and change to next question
-
-  }
-
+  },
   trivia: [
       { question: "What size is",
         choices: [10, 44, 17],
@@ -54,10 +67,8 @@ var playTrivia = {
         choices: [10, 44, 14],
         answerIndex: 0
       }
-
   ]
-
 }
+
 playTrivia.triviaQue();
 playTrivia.triviaChoices();
-playTrivia.triviaAnswer()
