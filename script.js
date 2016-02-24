27d8478db6386d9142a9efb4f89ca7faebb8ea24
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-24 09:55:10 -0500

    trivia questions and answers  added

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ var countersDiv = document.querySelectorAll(".flex-item:nth-child(3)");
  // get questions and answers from preloaded subject arrays
 
 var playTrivia = {
-  scienceQue: function() {
-    console.log(questionsDiv)
-      for (var i = 0; i < this.science.length; i++) {
-          questionsDiv[0].innerHTML +=
-           this.science[i].question;
+  triviaQue: function() {
+      for (var i = 0; i < this.trivia.length; i++) {
+          questionsDiv[0].innerHTML =
+           this.trivia[i].question;
       }
   },
 
-  scienceChoices: function() {
-    console.log(answersDiv)
-      for (var i = 0; i < this.science.length; i++) {
-          answersDiv[0].innerHTML += this.science[i].choices;
+  triviaChoices: function() {
+      for (var i = 0; i < this.trivia.length; i++) {
+        buildDeck: function() { // running continous loop in console
+         for (i = 0; i < values.length; j++) {
+              for (j = 0; j < suits.length; j++) {
+                game.deck.push(values[i]+suits[j]);
+
+          answersDiv[0].innerHTML = this.trivia[i].choices + "<input type='radio' />"
           // create and append buttons to choices
           // var button = document.createElement("BUTTON");
           // var myButton = answersDiv.appendChild(btn);
@@ -27,21 +30,22 @@ var playTrivia = {
   },
 
 // correctChoice: function(){
-//   for (var i=0; i<this.science.length; i++) {
+//   for (var i=0; i<this.trivia.length; i++) {
 //     // if user click correctIndex button, increase number of correct answers by 1, else increae number of incorrect annswer by 1 and change to next question
 //     if ()
 //   }
 // }
-  science: [
+  trivia: [
       { question: "What size is", choices: [10, 44, 15], correctIndex: 1 },
       { question: "???", choices: [0, 1, 2], correctIndex: 2 },
       { question: "Where is ", choices: [10, 44, 15], correctIndex: 1 },
       { question: "How do I", choices: [10, 44, 15], correctIndex: 0 }
+
   ]
 
 }
-playTrivia.scienceQue();
-playTrivia.scienceChoices();
+playTrivia.triviaQue();
+playTrivia.triviaChoices();
 
 
 
