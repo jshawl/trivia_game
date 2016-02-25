9746b9da04a36770f2ddf2cb1f419d7ca150d63e
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-25 10:49:31 -0500

    added more styling

--- script.js
+++ script.js
@@ -8,47 +8,40 @@ var countersDiv = document.getElementById("counter");
 var playTrivia = {
   totalAnswerCorrect: 0,
   totalQuestionsAsked: 0,
-  counter: 0,
+  counters: 0,
   // display questions
   triviaQue: function() {
-        var questions = this.trivia[this.counter].question;
+        var questions = this.trivia[this.counters].question;
         questionsDiv.innerHTML =  questions;
   },
 // display choices
   triviaChoices: function() {
     // Loop through, attach button and print each choice
-        for (var j=0; j<this.trivia[this.counter].choices.length; j++) {
-          choicesDiv.innerHTML += "<input type='radio' class="+ j + " name='myRadio' />" + this.trivia[this.counter].choices[j]
+        for (var j=0; j<this.trivia[this.counters].choices.length; j++) {
+          choicesDiv.innerHTML += "<input type='radio' class="+ j + " />" + this.trivia[this.counters].choices[j]
         }
-        var answer = this.trivia[this.counter].answerIndex
+        var answer = this.trivia[this.counters].answerIndex
         // add eventHandlers for all myRadio
         for (var i=0; i< choicesDiv.children.length; i++){
-          console.log(choicesDiv)
+          // console.log(choicesDiv)
           console.log(choicesDiv.children[i])
-          choicesDiv.children[i].addEventListener("click", playTrivia.triviaAnswer)
+          choicesDiv.children[i].addEventListener("click",playTrivia.triviaAnswers())
         }
-  },
-  triviaAnswer: function(){
-    console.log(this.children)
-    // var playerChoice = [choicesDiv[0].elements['myRadio']];
-    var playerChoiceRadio = choicesDiv.children
-    console.log("clicked radio:", playerChoiceRadio)
-    var playerChoice = playerChoiceRadio //???
-    console.log("playerChoice:", playerChoice)
+      },
+    triviaAnswers: function(){
+            var playerChoiceRadio = choicesDiv.children
+            // console.log("clicked radio:", playerChoiceRadio)
+            var playerChoice = playerChoiceRadio //???
+            countersDiv = this.counters++
+            if (playerChoiceRadio == this.trivia.correctIndex) {
+              totalAnswerCorrect++
+            }
+            else {
+              // totalQuestionsAsked++
+            }
 
-      // if (playerChoice = this.answer){
-      //   totalAnswerCorrect++;
-      // }
+            // if user click answerIndex button, increase number of correct answers by 1, else increae number of incorrect annswer by 1 and change to next question
 
-
-    // for (var i=0; i < playerChoice.length; i++) {
-    //   playerChoice[i].checked = function() {
-    //     console.log(playerChoice.index)
-    //     // if (this.trivia[this.counter].answerIndex = playerChoice.index)
-    //     //playerChoice
-    //   }
-    // }
-    // if user click answerIndex button, increase number of correct answers by 1, else increae number of incorrect annswer by 1 and change to next question
   },
   trivia: [
       { question: "What size is",
